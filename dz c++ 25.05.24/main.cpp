c1df9277fb4fe08b51545e96c1ffc5fd20d0732c
Vlad <vlad.orishchyn@gmail.com>
2024-06-28 20:36:23 +0300

    Initial Commit

---
+++ dz c++ 25.05.24/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  dz c++ 25.05.24
+//
+//  Created by Владислав Орищин on 28.06.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
