7992003c32b6492f772997970e3f9067fa40bb7f
Vlad <vlad.orishchyn@gmail.com>
2024-07-12 21:52:12 +0300

    Updated

--- dz c++ 25.05.24/main.cpp
+++ dz c++ 25.05.24/main.cpp
@@ -1,14 +1,56 @@
-//
-//  main.cpp
-//  dz c++ 25.05.24
-//
-//  Created by Владислав Орищин on 28.06.2024.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+// Реализуйте класс Дробь. Необходимо хранить числитель и знаменатель в качестве полей класса. Реализуйте методы для ввода данных , для выполнения арифметических операций (сложение, вычитание, умножение, деление, и т.д.) и для вывода данных.
+
+class Fraction
+{
+public:
+    int numerator;
+    int denominator;
+
+    void Input()
+    {
+        cout << "Enter numerator: " << endl;
+        cin >> numerator;
+        cout << "Enter denominator: " << endl;
+        cin >> denominator;
+    }
+    
+    void Sum(int x)
+    {
+        numerator += (x * denominator);
+        cout << " Result of Addition is " << numerator << "/" << denominator << endl;
+    }
+    
+    void Sub(int x)
+    {
+        numerator -= (x * denominator);
+        cout << " Result of Subtraction is " << numerator << "/" << denominator << endl;
+    }
+    
+    void Mult(int x)
+    {
+        numerator *= x;
+        cout << " Result of Multiplication is " << numerator << "/" << denominator << endl;
+    }
+    
+    void Div(int x)
+    {
+        denominator *= x;
+        cout << " Result of Division is " << numerator << "/" << denominator << endl;
+    }
+
+};
+
+int main()
+{
+    Fraction a;
+    a.Input();
+    a.Sum(1);
+    a.Sub(2);
+    a.Mult(3);
+    a.Div(4);
 }
+
+
